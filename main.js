c9d8975be98938d6c1e6cc6d0bbdc4787a339d32
Yonny Rodriguez <jhrodrigu@hotmail.com>
2021-03-22 16:50:05 -0500

    Node

---
+++ main.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen(5000, () => {
+    console.log("servidor en 5000")
+})
