d03127f34c9fea94f391366d0c0372f4c1a0d867
Yonny Rodriguez <jhrodrigu@hotmail.com>
2021-03-23 07:29:32 -0500

    nuevo formulario

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const app = express()
+const path = require('path')
 
 app.get('/', (req, res, next) => {
     res.sendFile(__dirname + "/index.html")
 })
 
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/datos', (req, res) => {
+
+app.get('/enviar', (req, res) => {
     console.log(req.body);
-    res.send('recivido');
+    res.send('recibido');
 })
 
 
