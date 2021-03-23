0e32b35c10975fa11916c7ae57cb55fa5298d35b
Yonny Rodriguez <jhrodrigu@hotmail.com>
2021-03-22 20:02:36 -0500

    recibiendo datos a node

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const express = require("express")
-const http = require('http')
 const app = express()
 
 app.get('/', (req, res, next) => {
     res.sendFile(__dirname + "/index.html")
+})
+
+
+app.get('/datos', (req, res) => {
+    console.log(req.body);
+    res.send('recivido');
 })
 
 
