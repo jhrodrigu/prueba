e31298217ba851693ddbeadb56c7eff233debcaf
dariza <david.ariza@pi.edu.co>
2021-03-23 12:20:52 -0500

    se crea el db.js para interactuar con la base de datos, sin modelos, solo la conexion

---
+++ db.js
@@ -0,0 +1,22 @@
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database(':memory:');
+
+db.run('CREATE TABLE usuarios(nombre text, apellido text);',(err)=>{
+    if(err){
+        return console.log(err.message);
+    }
+    else{
+        console.log('Tabla usuarios Creada')
+    }
+})
+
+db.close((err)=>{
+    if (err){
+        return console.log('bd no cerro :'+err.message)
+    }
+    else{
+        console.log('conexion cerrada a DB')
+    }
+});
+
