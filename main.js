32f8dbf0749955daf27765dc17a38b37548e1969
Yonny Rodriguez <jhrodrigu@hotmail.com>
2021-03-22 19:35:15 -0500

    parte de node

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const http = require('http')
 const app = express()
 
 app.get('/', (req, res, next) => {
     res.sendFile(__dirname + "/index.html")
 })
+
 
 app.listen(5000, () => {
     console.log("servidor en 5000")
